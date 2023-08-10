ec8e3b5ddc2ed3fce4e10f59364be53f3e79d706
lenovo <lenovo@LAPTOP-BLNTEBH7>
2023-08-10 10:18:33 +0530

    changes for glossry

---
+++ movies/New folder/app.js
@@ -0,0 +1,13 @@
+`<p class="title">${value}</p>
+            <div class="btn-container">
+              <!-- edit btn -->
+              <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+              </button>
+              <!-- delete btn -->
+              <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>
+          `;
+    
